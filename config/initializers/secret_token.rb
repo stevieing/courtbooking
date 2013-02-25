18db77c8eb7b9897eb5e766c740fe7642e89b2b4
Stephen Inglis <stevieing@sky.com>
2013-02-25 21:52:37 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Courtbooking::Application.config.secret_token = 'a85be003c4fd16ba0dc1066bfb52bf7bb90194010c6c4d578a5f8c6ef099846069e08b5719cfe2813c91b61b3a21d600e990846dfa67e17411c64ed9fc599510'
