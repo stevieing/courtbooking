7d51432a2044211ffa630426e4f3907064c2240d
Stephen Inglis <stevieing@sky.com>
2014-03-17 14:53:23 +0000

    move parameters processing out to module for admin users.

--- app/forms/manage_user_form.rb
+++ app/forms/manage_user_form.rb
@@ -4,17 +4,20 @@
 class ManageUserForm
 
 	include ManageForm
+  include ParametersProcessor
+
 	set_model :user, ACCEPTED_ATTRIBUTES.user
 	delegate :permissions, to: :user
 
 	validate :verify_user
 
 	def process_params(params)
-		params.dup.tap do |p|
-			if p[:password].blank? && p[:password_confirmation].blank? && persisted?
-				p.delete_all(:password, :password_confirmation)
-			end
-		end
+    persisted? ? process_parameters(params) : params
+		# params.dup.tap do |p|
+		# 	if p[:password].blank? && p[:password_confirmation].blank? && persisted?
+		# 		p.delete_all(:password, :password_confirmation)
+		# 	end
+		# end
 	end
 
   def include_action?(allowed_action)
