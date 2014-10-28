65d96c2f2052adce6d0aca9b0b60de5ad043df91
Stephen Inglis <stevieing@sky.com>
2014-10-28 19:58:44 +0000

    more changes to default email.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,4 @@
 require 'mail'
 require 'development_mail_interceptor'
 
-Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development? || Rails.env.staging?
+Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
